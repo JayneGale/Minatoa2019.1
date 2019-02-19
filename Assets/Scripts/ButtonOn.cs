b1d8b0dc929e3eb80adb131604b84e629d1e6f64
Jayne Gale <jaynegale@gmail.com>
2019-02-19 23:56:36 +1300

    Scaled scene with several rooms

---
+++ Assets/Scripts/ButtonOn.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonOn : MonoBehaviour {
+	
+	public GameObject buttonToPress;
+	public GameObject objectButtonOperates;
+	public bool buttonIsActivated;
+	MeshRenderer meshRenderer;
+	public Texture buttonOn;
+	public Texture buttonOff;
+	Animator animator;
+
+	// Use this for initialization
+	void Start () 
+	{
+		meshRenderer = GetComponent<MeshRenderer> ();
+		meshRenderer.material.SetTexture ("_MainTex", buttonOff);
+		animator = gameObject.GetComponent<Animator>();
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.T))
+		{
+			//Send the message to the Animator to activate the trigger parameter named "ButtonPress"
+			animator.SetTrigger("ButtonPress");
+			ToggleButton ();
+		}
+		if (Input.GetKeyDown (KeyCode.Equals)) 
+		{
+			animator.SetTrigger ("ButtonPress");
+			ActivateButton ();
+		}	 
+		if (Input.GetKeyDown (KeyCode.Minus)) 
+		{
+			animator.SetTrigger ("ButtonPress");
+			DeactivateButton ();
+		}
+		objectButtonOperates.SetActive (buttonIsActivated);
+
+	}
+
+	public void ActivateButton()
+	{
+		meshRenderer.material.SetTexture ("_MainTex", buttonOn);
+		buttonIsActivated = true;			
+	}
+
+	public void DeactivateButton()
+	{
+		meshRenderer.material.SetTexture ("_MainTex", buttonOff);
+		buttonIsActivated = false;
+		objectButtonOperates.SetActive (false);
+	}
+
+	public void ToggleButton()
+	{
+		if (meshRenderer.material.GetTexture ("_MainTex") == buttonOff) 
+		{
+			meshRenderer.material.SetTexture ("_MainTex", buttonOn);
+			buttonIsActivated = true;
+
+			return;
+		} 
+		else if (meshRenderer.material.GetTexture ("_MainTex") == buttonOn) 
+		{
+			meshRenderer.material.SetTexture ("_MainTex", buttonOff);
+			buttonIsActivated = false;
+			return;
+		} 
+	}
+}
---
+++ Assets/Scripts/SpinOrrery.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinOrrery : MonoBehaviour {
+
+	public Vector3 rotationSpeed;
+	public GameObject orreryButton;
+	bool spinning = false;
+
+	// Use this for initialization
+	void Start () {
+		ButtonClicker buttonClicker; 
+		buttonClicker = orreryButton.GetComponent<ButtonClicker>();
+		buttonClicker.OnClicked += Button_OnClicked;
+	}
+
+	void Button_OnClicked (bool buttonState)
+	{
+		spinning = buttonState;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (spinning) {
+			gameObject.transform.eulerAngles += rotationSpeed;
+		}
+	}
+	}
